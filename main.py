b93b09ee760cef55472ebf091ab1693f469fa52e
Matthew Earl <git@matthewearl.com>
2025-04-09 07:33:00 +0100

    put get_base_file in its own function

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import io
+
 from js import console, document
 from pydem.messages import ProtocolVersion
 
 
-async def run_superimpose(event):
+async def get_base_file():
     base_input = document.getElementById("baseDemInput")
     if not base_input.files or len(base_input.files) == 0:
         document.getElementById("downloadLink").innerHTML = "Please select a base DEM file."
         return
     base_file = base_input.files.item(0)
+    base_bytes = (await base_file.arrayBuffer()).to_bytes()
+    return io.BytesIO(base_bytes)
 
-    array_buf = await base_file.arrayBuffer()
-    console.log(str(array_buf.to_bytes()))
+
+async def run_superimpose(event):
+    base_file = await get_base_file()
+    console.log(str(base_file.getvalue()))
